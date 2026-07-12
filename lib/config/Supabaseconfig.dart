227fc52f8bbee903410d4e7866f1b2e96b273d10
bittu <bittuss1203@gmail.com>
2026-07-12 11:08:37 +0530

    clean repo , file structure

---
+++ lib/config/Supabaseconfig.dart
@@ -0,0 +1,5 @@
+//supabase
+class SupabaseConfig {
+  static const String url = 'https://sljkvdxcscmqpasksxyu.supabase.co';
+  static const String anonKey = 'sb_publishable_nDsXI5IJytD2DUcSx56vDA_f8sQseIo';
+}
